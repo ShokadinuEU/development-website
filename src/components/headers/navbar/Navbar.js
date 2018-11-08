2dc120e53d87fd2c85d4b2aa5fce097d233fd356
Timotei Dumitru <dumitru.timy@gmail.com>
2018-11-08 01:32:35 +0000

    implement the basic structure of top navbar and style it

---
+++ src/components/headers/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './navbar.scss'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-main">
+        <div className="navbar-wrapper">
+          <div className="brand"><img src="./../../data/images/logo/brand-logo.png" alt="nav-brand-img"/></div>
+          <div className="navbar-links">
+            <div className="drop-down">Browse <span>&#187;</span></div>
+            <div className="nav-info">How it Work</div>
+            <div className="navbar-button-main">
+              <a href="/" className="navbar-button-link">Login</a></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
