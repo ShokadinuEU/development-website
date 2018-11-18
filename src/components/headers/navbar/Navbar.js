549eacc7a6e08a0b8bce4b180ab231d895bf535c
Timotei Dumitru <dumitru.timy@gmail.com>
2018-11-18 14:59:17 +0000

    got rid of unuse code in the navbar section

--- src/components/headers/navbar/Navbar.js
+++ src/components/headers/navbar/Navbar.js
@@ -3,18 +3,6 @@ import './navbar.scss'
 import Login from './Login';
 
 class Navbar extends Component {
-  state = { 
-    show: false 
-  }
-
-  showModal = () => {
-    this.setState({ show: true })
-  }
-
-  hideModal = () => {
-    this.setState({ show: false })
-  }
-
   render() {
     return (
       <div className="navbar-main">
@@ -22,7 +10,7 @@ class Navbar extends Component {
           <div className="brand"><img src="./data/images/logo/navbar/brand-logo.png" alt="nav-brand-img"/></div>
           <div className="navbar-links">
             <div className="drop-down">Browse 
-            <span><img src="./data/images/logo/header/right-arrow.png" alt="" className="down-arrow-navbar"/></span>
+            <span><img src="./data/images/logo/header/right-arrow.png" alt="down-arrow-header" className="down-arrow-navbar"/></span>
             </div>
             <div className="nav-info">How it Work</div>
             <div className="navbar-button-main">
